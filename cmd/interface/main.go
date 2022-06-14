0686dd811d4ac09aa87d7b0c719f731004726ccf
zhuwenkai <zhuwenkai@egova.com.cn>
2022-06-14 14:33:46 +0800

    接口可以和具体值直接比较,判断依据依次是类型/具体值

--- cmd/interface/main.go
+++ cmd/interface/main.go
@@ -10,7 +10,36 @@ type obj struct {
 }
 
 func main() {
+	p()
+	println("=================")
+	cmp()
+}
+
+func p() {
 	var s obj
 
-	fmt.Printf("%+v", s)
+	fmt.Printf("%+v\n", s)
+}
+
+func cmp() {
+	var i interface{}
+	var age int = 322
+	var address string = "abc"
+
+	i = age
+	i = nil
+	if i == 12 {
+		println("i=12")
+	} else if i == 32 {
+		println("i=32")
+	} else if i == "abc" {
+		println("i=abc")
+	} else if i == nil {
+		println("i=nil")
+	}
+
+	i = address
+	if i == "abc" {
+		println("i=abc")
+	}
 }
