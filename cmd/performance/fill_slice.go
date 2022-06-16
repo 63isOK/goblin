98dc6df0d7418a862d1e8a340496a164a16ceccc
zhuwenkai <zhuwenkai@egova.com.cn>
2022-06-16 17:41:46 +0800

    测试覆盖率

--- cmd/performance/fill_slice.go
+++ cmd/performance/fill_slice.go
@@ -1,30 +1,31 @@
 package performance
 
-// var src = []any{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 1, 1, 1, 1, 11, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11, 1, 1, 1, 1, 1, 1, 11, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11, 1, 2, 2, 2, 2, 22, 2, 2, 2, 22, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11, 1}
-var src = []any{"string", "1", 1, 1, 1, 1, 11, 1, 1, 1, "abc"}
+var src = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 1, 1, 1, 1, 11, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11, 1, 1, 1, 1, 1, 1, 11, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11, 1, 2, 2, 2, 2, 22, 2, 2, 2, 22, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11, 1}
 
-func WithAppend() []any {
-	dst := make([]any, 0)
+// var src = []int{"string", "1", 1, 1, 1, 1, 11, 1, 1, 1, "abc"}
+
+func WithAppend() []int {
+	dst := make([]int, 0)
 	for _, v := range src {
-		dst = append(dst, v)
+		dst = append(dst, v+1)
 	}
 
 	return dst
 }
 
-func WithAppendAndLength() []any {
-	dst := make([]any, 0, len(src))
+func WithAppendAndLength() []int {
+	dst := make([]int, 0, len(src))
 	for _, v := range src {
-		dst = append(dst, v)
+		dst = append(dst, v+1)
 	}
 
 	return dst
 }
 
-func WithAssign() []any {
-	dst := make([]any, len(src))
+func WithAssign() []int {
+	dst := make([]int, len(src))
 	for i, v := range src {
-		dst[i] = v
+		dst[i] = v + 1
 	}
 
 	return dst
--- cmd/performance/fill_slice_test.go
+++ cmd/performance/fill_slice_test.go
@@ -6,6 +6,14 @@ import (
 	. "github.com/63isOK/goblin/cmd/performance"
 )
 
+func TestWithAppend(t *testing.T) {
+	got := WithAppend()
+
+	if len(got) != 1 {
+		t.Fatalf("预计长度大于1,实际%d", len(got))
+	}
+}
+
 func BenchmarkWithAppend(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		WithAppend()
